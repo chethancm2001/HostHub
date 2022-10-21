0412e019ae61451717c78e247fe1f75e76072199
chethancm2001 <chethancm2001kadur@gmail.com>
2022-10-21 10:58:08 +0530

    added auth

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,14 @@
 const router  = require('express').Router()
 const register = require('../controllers/register')
 const gitcontroller = require('../controllers/gitcontroller')
-
+const login = require('../controllers/login')
 router.get('/',(req,res)=>{
     res.send('working from the router')
 })
 
 router.post('/register',register)
+router.post('/login',login)
 
 router.post('/git-clone',gitcontroller)
+
 module.exports = router
